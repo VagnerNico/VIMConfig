6884fa99878553a9b050acf99d4b50f4d4a3bf06
Vagner José Nicolodi <vagnernico@gmail.com>
2022-06-29 20:43:55 -0300

    Removed copilot from lvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,83 +1,84 @@
--- General
-vim.opt.relativenumber = true
-lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "dracula"
-lvim.autocommands.custom_groups = {
-  { "CursorHold", "*", "lua vim.diagnostic.open_float()" },
-}
-
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = ","
-
-lvim.keys.insert_mode[","] = ","
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.visual_mode["S"] = ""
-
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- General
+vim.opt.relativenumber = true
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "dracula"
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = ","
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = { "*" },
+  -- enable wrap mode for json files only
+  command = "lua vim.diagnostic.open_float()",
+})
+
+lvim.keys.insert_mode[","] = ","
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.visual_mode["S"] = ""
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.dap.active = true
-
--- if you don't want all the parsers change this to a table of the ones you want
-
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
-
--- ESLint
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  {
-    exe = "eslint_d",
-    filetypes = {
-      "javascriptreact",
-      "javascript",
-      "typescriptreact",
-      "typescript",
-      "vue",
-    },
-  },
-}
-
-local code_actions = require "lvim.lsp.null-ls.code_actions"
-code_actions.setup {
-  {
-    exe = "eslint_d",
-    filetypes = {
-      "javascriptreact",
-      "javascript",
-      "typescriptreact",
-      "typescript",
-      "vue",
-    },
-  },
-}
-
--- Additional Plugins
-lvim.plugins = {
-  { "tpope/vim-surround" },
-  { "dracula/vim", as = "dracula" },
-  { "mattn/emmet-vim" },
-  { "github/copilot.vim" },
+
+-- if you don't want all the parsers change this to a table of the ones you want
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.indent = { enable = true, disable = { "yaml", "python" } }
+
+-- ESLint
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    exe = "eslint_d",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+      "vue",
+    },
+  },
+}
+
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {
+  {
+    exe = "eslint_d",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+      "vue",
+    },
+  },
+}
+
+-- Additional Plugins
+lvim.plugins = {
+  { "tpope/vim-surround" },
+  { "dracula/vim", as = "dracula" },
+  { "mattn/emmet-vim" },
+  -- { "github/copilot.vim" },
   {
     "jose-elias-alvarez/nvim-lsp-ts-utils",
     ft = {
@@ -109,99 +110,111 @@ lvim.plugins = {
       "Glgrep",
       "Gedit"
     },
-    ft = {"fugitive"}
+    ft = { "fugitive" }
   },
-}
-
--- Emmet configuration
-vim.g.user_emmet_leader_key = ','
-vim.g.user_emmet_settings = {
-  javascript = {
-    extends = 'jsx',
-  },
-}
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
-
-vim.list_extend(lvim.lsp.override, { "eslint_d", "tsserver" })
-
-local servers = require("nvim-lsp-installer.servers")
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd "highlight default link gitblame SpecialComment"
+      vim.g.gitblame_enabled = 0
+    end,
+  },
+}
+
+-- Emmet configuration
+vim.g.user_emmet_leader_key = ','
+vim.g.user_emmet_settings = {
+  javascript = {
+    extends = 'jsx',
+  },
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "eslint_d", "tsserver" })
+
+local servers = require("nvim-lsp-installer.servers")
 local status_ok, ts_utils = pcall(require, "nvim-lsp-ts-utils")
-local eslint_available, eslint_server = servers.get_server("eslint")
-local tsserver_available, tsserver_server = servers.get_server("tsserver")
-
-if not status_ok then
-  vim.cmd [[ packadd nvim-lsp-ts-utils ]]
-  ts_utils = require "nvim-lsp-ts-utils"
-end
-
-if eslint_available and tsserver_available then
-  local lvimLsp = require("lvim.lsp")
-  local eslintOptions = {
-    on_attach = function(client, bufnr)
-        client.resolved_capabilities.document_formatting = true
-        lvimLsp.common_on_attach(client, bufnr)
-    end,
-    settings = {
-      format = {
-        enable = true,
-      },
-    },
-    cmd_env = eslint_server:get_default_options().cmd_env,
-  }
-  
-  local tsserverOptions = {
-    on_attach = function(client, bufnr)
-        client.resolved_capabilities.document_formatting = false
-        ts_utils.setup {
-          debug = false,
-          disable_commands = false,
-          enable_import_on_completion = false,
-          import_all_timeout = 5000, -- ms
-
-          -- parentheses completion
-          complete_parens = false,
-          signature_help_in_parens = false,
-
-          -- update imports on file move
-          update_imports_on_move = true,
-          require_confirmation_on_move = false,
-          watch_dir = nil,
-        }
-        ts_utils.setup_client(client)
-        lvimLsp.common_on_attach(client, bufnr)
-    end,
-    init_options = ts_utils.init_options,
-    on_init = lvimLsp.common_on_init,
-    settings = {
-      format = {
-        enable = false,
-      },
-    },
-    cmd_env = tsserver_server:get_default_options().cmd_env,
-  }
-  
-  require("lvim.lsp.manager").setup("eslint", eslintOptions)
-  require("lvim.lsp.manager").setup("tsserver", tsserverOptions)
-end
-
---Copilot overrides
-local cmp = require "cmp"
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_tab_fallback = ""
-lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  else
-    local copilot_keys = vim.fn["copilot#Accept"]()
-    if copilot_keys ~= "" then
-      vim.api.nvim_feedkeys(copilot_keys, "i", true)
-    else
-      fallback()
-    end
-  end
-end
+local eslint_available, eslint_server = servers.get_server("eslint")
+local tsserver_available, tsserver_server = servers.get_server("tsserver")
+
+if not status_ok then
+  vim.cmd [[ packadd nvim-lsp-ts-utils ]]
+  ts_utils = require "nvim-lsp-ts-utils"
+end
+
+if eslint_available and tsserver_available then
+  local lvimLsp = require("lvim.lsp")
+  local eslintOptions = {
+    on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = true
+      lvimLsp.common_on_attach(client, bufnr)
+    end,
+    settings = {
+      format = {
+        enable = true,
+      },
+    },
+    cmd_env = eslint_server:get_default_options().cmd_env,
+  }
+
+  local tsserverOptions = {
+    on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      ts_utils.setup {
+        debug = false,
+        disable_commands = false,
+        enable_import_on_completion = false,
+        import_all_timeout = 5000, -- ms
+
+        -- parentheses completion
+        complete_parens = false,
+        signature_help_in_parens = false,
+
+        -- update imports on file move
+        update_imports_on_move = true,
+        require_confirmation_on_move = false,
+        watch_dir = nil,
+      }
+      ts_utils.setup_client(client)
+      lvimLsp.common_on_attach(client, bufnr)
+    end,
+    init_options = ts_utils.init_options,
+    on_init = lvimLsp.common_on_init,
+    settings = {
+      format = {
+        enable = false,
+      },
+    },
+    cmd_env = tsserver_server:get_default_options().cmd_env,
+  }
+
+  require("lvim.lsp.manager").setup("eslint", eslintOptions)
+  require("lvim.lsp.manager").setup("tsserver", tsserverOptions)
+end
+
+--Copilot overrides
+-- local cmp = require "cmp"
+-- vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_assume_mapped = true
+-- vim.g.copilot_tab_fallback = ""
+-- lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
+--   if cmp.visible() then
+--     cmp.select_next_item()
+--   else
+--     local copilot_keys = vim.fn["copilot#Accept"]()
+--     if copilot_keys ~= "" then
+--       vim.api.nvim_feedkeys(copilot_keys, "i", true)
+--     else
+--       fallback()
+--     end
+--   end
+-- end
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { exe = "black", filetypes = { "python" } }
+}
